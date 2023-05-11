544f6bb4bbe505ec43e613d135fd56c53182c3fc
naatii <jcorber855@g.educaand.es>
2023-05-11 09:59:55 +0200

    final

--- ObtenerLogsServer.py
+++ ObtenerLogsServer.py
@@ -17,28 +17,27 @@ sftp = paramiko.SFTPClient.from_transport(transport)
 # Descarga del archivo
 remote_path = '/var/log/nginx/access.log'
 local_path = './access.log'
-sftp.get(remote_path, local_path)
-
-# Cierre de la conexión SFTP
-sftp.close()
-transport.close()
-archivo = ""
-a = open('access.log', 'r')
-f = open('serverLog.html','w')
-for line in a:
-    f.write("""<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <div>"""+line+"""</div>
-    </body>
-    </html>
-    """)
 webbrowser.open_new_tab('serverLog.html')
-a.close()
-f.close()
+
+while True:
+    sftp.get(remote_path, local_path)
+
+    # Cierre de la conexión SFTP
+
+    a = open('access.log', 'r')
+    f = open('serverLog.html','w')
+    for line in a.readlines()[-10:]:
+        f.write("""<!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="refresh" content="2">
+                <title>Document</title>
+            </head>
+            <body>
+                <div>"""+line+"""</div>
+            </body>
+            </html>
+            """)
